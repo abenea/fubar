f1ae99da54e807a81acc22fb304745cc5bbd952a
bugdone <bugdone@gmail.com>
2012-10-31 18:04:55 -0400

    Bugfix: Entering directories to be monitored with "/" at the end now don't refuse to handle events in those directories.

--- ui/librarypreferencesdialog.cpp
+++ ui/librarypreferencesdialog.cpp
@@ -16,8 +16,9 @@ void LibraryPreferencesDialog::accept()
     QSet<QString> current_set;
     QStringList current_list = libraryPaths->text().split(FOLDER_SEPARATOR);
     foreach (QString path, current_list) {
-        if (path.size() > 0)
-            current_set.insert(path);
+        if (path.size() > 0) {
+            current_set.insert(QFileInfo(path).canonicalFilePath());
+        }
     }
     QSet<QString> old_set;
     QStringList old_list = library_.getMusicFolders();
